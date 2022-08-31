f3422fa1a4b282c88a62288b589bc3c7e10f395f
brianwilkinson <wilkibrian@gmail.com>
2022-08-31 16:20:33 -0400

    Finish Tempeature Conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = 5/9 * (f-32);
+  return Math.round(c*10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = 9/5 * c + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
