79f19f36aa2d6886399da37b4b9e9d24863834ad
brianwilkinson <wilkibrian@gmail.com>
2022-09-08 14:49:18 -0400

    Finish Calculator problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1+num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const sum = function(myArr) {
+	const totalSum = myArr.reduce((prevVal, nextVal) => prevVal + nextVal, 0);
+  return totalSum;
+};
+
+const multiply = function(myArr) {
+  const totalProd = myArr.reduce((prevVal, nextVal) => prevVal * nextVal, 1);
+  return totalProd;
+};
+
+const power = function(base, exp) {
+  if (exp === 0) return 1;
+  else return base * power(base, exp-1);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+  else return num * factorial(num - 1);
 };
 
 // Do not edit below this line
