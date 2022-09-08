73b52e1590d6a2dc1dcc8f5966493bf3c4d9bf62
brianwilkinson <wilkibrian@gmail.com>
2022-09-08 15:37:41 -0400

    Finish Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/\W+/g, '').toLowerCase();
+    for (let i=0; i<str.length/2; i++) {
+        if (str[i] !== str[str.length-i-1]) return false;
+    }
+    return true;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
