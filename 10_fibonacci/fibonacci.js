4c903745662029df7289ae9c40a72a019a1a6179
brianwilkinson <wilkibrian@gmail.com>
2022-09-08 15:50:48 -0400

    Finish Fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) return "OOPS";
+    if (num <=2) return 1;
+    a = 1;
+    b = 1;
+    while (num-2 > 0) {
+        c = a + b;
+        a = b;
+        b = c;
+        num--;
+    }
+    return c;
 };
 
 // Do not edit below this line
