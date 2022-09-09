1ab16c712ec3433f835d0c948961ad7677b74c3e
brianwilkinson <wilkibrian@gmail.com>
2022-09-09 11:39:27 -0400

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(myObj) {
+    const sorted = myObj.sort(function(a, b) {
+        aDied = !a.yearOfDeath ? 2022 : a.yearOfDeath;
+        bDied = !b.yearOfDeath ? 2022 : b.yearOfDeath;
+        age1 = aDied - a.yearOfBirth;
+        age2 = bDied - b.yearOfBirth;
+        return age1 > age2 ? -1 : 1;
+    })
+    return sorted[0];
 };
 
 // Do not edit below this line
